39a385c79fd1d194cf163ce66cbc3f4cb73ba1ac
JAW12 <jem.angkasa91@gmail.com>
2020-11-17 18:40:19 +0700

    CLOSE #3, CLOSE #5
    1. Selesai Import - Import lagi
    2. Menu Utama Design Beres
    3. Help Page sudah bisa tampil dan tutup

---
+++ Assets/Script/canvasSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canvasSettings : MonoBehaviour
+{
+    public GameObject panelHelp;
+    public GameObject panelMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelHelp.SetActive(false);
+    }
+
+    public void showHelp(){
+        panelHelp.SetActive(true);
+    }
+
+    public void closeHelp(){
+        panelHelp.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/closeHelp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closeHelp : MonoBehaviour
+{
+    private void Update() {
+        HideIfClickedOutside(gameObject);
+    }
+    private void HideIfClickedOutside(GameObject panel) {
+        if (Input.GetMouseButton(0) && panel.activeSelf && 
+            !RectTransformUtility.RectangleContainsScreenPoint(
+                panel.GetComponent<RectTransform>(), 
+                Input.mousePosition, 
+                Camera.main)) {
+            panel.SetActive(false);
+        }
+    }
+}
