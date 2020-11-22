8a388e0f8db09d35987e2fb40e68446355555957
JAW12 <jem.angkasa91@gmail.com>
2020-11-22 07:16:54 +0700

    CLOSE #7, FIX #9
    1. Buat scene
    2. Buat map
    3. Buat shop tapi masih progress
    4. Sudah bisa pindah dari Play Button ke Scene Game

--- Assets/Script/canvasSettings.cs
+++ Assets/Script/canvasSettings.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class canvasSettings : MonoBehaviour
 {
     public GameObject panelHelp;
     public GameObject panelMenu;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +19,10 @@ public class canvasSettings : MonoBehaviour
 
     public void closeHelp(){
         panelHelp.SetActive(false);
+    }
+
+    public void startGame(){
+        SceneManager.LoadScene("Game");
     }
 
     // Update is called once per frame
