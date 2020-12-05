9162671c2474aa45a6e91ee4551ae0d234fbf1cb
JAW12 <jem.angkasa91@gmail.com>
2020-12-05 08:23:36 +0700

    FIX LOADING SCREEN
    Pakai pengecekan kalau sudah full baru pindah scene

--- Assets/Script/canvasLoading.cs
+++ Assets/Script/canvasLoading.cs
@@ -15,26 +15,27 @@ public class canvasLoading : MonoBehaviour
     public GameObject progress;
     // Start is called before the first frame update
     float ctr = 0.03f;
-    private IEnumerator Start()
+    private void Start()
     {
         Cursor.visible = false;
-        yield return new WaitForSeconds(2.7f);
-        if(sceneLoad == 0){
-            StoryAssistant.menang = false;
-            SceneManager.LoadScene("Storyline");
-        }
-        else if(sceneLoad == 1){
-            SceneManager.LoadScene("Tutorial");
-        }
-        else if(sceneLoad == 2){
-            SceneManager.LoadScene("Game");
-        }
     }
 
     private void Update() {
         progress.transform.localScale = new Vector3(ctr/3, progress.transform.localScale.y, 1);
         if(ctr / 3 < 1f){
             ctr += 0.01f;
+        }
+        else{
+            if(sceneLoad == 0){
+                StoryAssistant.menang = false;
+                SceneManager.LoadScene("Storyline");
+            }
+            else if(sceneLoad == 1){
+                SceneManager.LoadScene("Tutorial");
+            }
+            else if(sceneLoad == 2){
+                SceneManager.LoadScene("Game");
+            }
         }
     }
 }
